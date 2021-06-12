5bff95e55e18062c350fb406ea5535fcbaa8aae1
godzilla652 <nikitamelnyk@yahoo.com>
2021-06-12 21:00:15 +0300

    created tcp server

---
+++ index.js
@@ -0,0 +1,35 @@
+var net = require('net');
+
+var server = net.createServer();
+server.on('connection', handleConnection);
+
+server.listen(9000, function() {
+  console.log('server listening to %j', server.address());
+});
+
+
+function handleConnection(conn) {
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  console.log('new client connection from %s', remoteAddress);
+
+  conn.setEncoding('utf8');
+
+	conn.on('data', onConnData);
+	conn.once('close', onConnClose);
+	conn.on('error', onConnError);
+
+	function onConnData(data) {
+		// name@gmail.com-::-subject-::-text
+
+
+		// asnswer client
+	 	// conn.write(d.toUpperCase());
+	}
+	 function onConnClose() {
+	    console.log('connection from %s closed', remoteAddress);
+  	}
+		function onConnError(err) {
+			console.log('Connection %s error: %s', remoteAddress, err.message);
+	}
+
+}
